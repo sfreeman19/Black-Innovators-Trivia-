c7354f8aedbe015dd114e59acc4efba7032744c7
sfreeman19 <savana.freeman13@gmail.com>
2016-06-28 00:42:38 +0900

    Project's Initial Commit

---
+++ www/scripts/myScript.js
@@ -0,0 +1,20 @@
+// This is a JavaScript file
+function Incorrect(){
+    //Grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("wrong1");
+    wrongAnswer.style.color = "red";
+    
+}
+function Incorrect2(){
+    var wrongAnswer = document.getElementById("wrong2");
+    wrongAnswer.style.color = "red";
+  
+}
+function Incorrect3(){
+     var wrongAnswer = document.getElementById("wrong3");
+     wrongAnswer.style.color = "red"
+}
+function Correct(){
+    var correctAnswer = document.getElementById("correct")
+    correctAnswer.style.color = "green"
+}
